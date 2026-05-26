8df530fb0c58db6ab7944e18f688154c9a6f1a8a
Kati Dnts <katidnts@gmail.com>
2026-05-25 23:46:59 -0300

    test: adiciona teste de buscarCep com CEP válido no EnderecoControllerTest

---
+++ src/test/java/br/com/katidantas/smartdelivery/endereco/EnderecoControllerTest.java
@@ -0,0 +1,69 @@
+package br.com.katidantas.smartdelivery.endereco;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EnderecoController.class)
+public class EnderecoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private CepService cepService;
+
+    @Test
+    @DisplayName("Deve retornar um endereço quando cep for válido")
+    void deveRetornarEndereco_QuandoCepValido() throws Exception {
+
+        //Given
+
+        String cep = "22220001";
+
+        EnderecoParcialDTO enderecoParcialDTO = new EnderecoParcialDTO(
+                "22220001",
+                "Rua do Catete",
+                "Catete",
+                "Rio de Janeiro",
+                "RJ"
+        );
+
+        when(cepService.buscarCep(cep)).thenReturn(enderecoParcialDTO);
+
+        //When
+
+        String responseBody = mockMvc.perform(get("/endereco/{cep}", cep))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+
+        EnderecoParcialDTO endereco = objectMapper.readValue(responseBody, EnderecoParcialDTO.class);
+
+        //Then
+
+        assertThat(endereco.cep()).isEqualTo(enderecoParcialDTO.cep());
+        assertThat(endereco.logradouro()).isEqualTo(enderecoParcialDTO.logradouro());
+        assertThat(endereco.bairro()).isEqualTo(enderecoParcialDTO.bairro());
+        assertThat(endereco.localidade()).isEqualTo(enderecoParcialDTO.localidade());
+        assertThat(endereco.uf()).isEqualTo(enderecoParcialDTO.uf());
+
+        verify(cepService).buscarCep(cep);
+    }
+
+}
